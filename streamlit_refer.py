a80bc681eef284b0c382a006af649f185218e865
richpie <pie0108@naver.com>
2024-08-23 10:22:04 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -24,7 +24,8 @@ def main():
     page_title="DirChat",
     page_icon=":books:")
 
-    st.title("_Private Data :red[QA Chat]_ :books:")
+    # 타이틀 제목을 변경
+    st.title("_DAESUNG DATA :red[대성에너지]_ :books:")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -53,7 +54,7 @@ def main():
 
     if 'messages' not in st.session_state:
         st.session_state['messages'] = [{"role": "assistant", 
-                                        "content": "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
+                                        "content": "안녕하세요! 대성에너지 GPT 입니다. 궁금하신 것이 있으시면 물어봐주세요!"}]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
